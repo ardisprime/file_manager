d5f856bdf6044096e972024bd090895f6c2ecc35
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2025-04-17 12:32:12 +0200

    addded makefile and path management

--- main.lua
+++ main.lua
@@ -1,18 +1,7 @@
 
-window = require "src/window"
-
-vector = require "src/vector"
-
-print("vector:")
-print(vector:get("a") )
-
-window.test_print("hello")
-
-print(window.get_pos("x") )
-window.set_pos("x", "10")
-print(window.get_pos("x") )
-
-print(window.get_size("h") )
-window.set_size("h", "5")
-print(window.get_size("h") )
+-- windows
+--  o window
+--   o position -> vector
+--   o size -> vector
+--   o buffer
 
---
+++ paths.lua
@@ -0,0 +1,10 @@
+  
+-- chunk to keep track of paths
+
+local paths =  {}
+
+paths.vector = "src/vector"
+paths.window = "src/window"
+
+return paths
+
