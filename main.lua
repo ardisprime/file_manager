efbda79cb8a0a5d5b2de33bc41c14d2e9120d626
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2025-04-17 15:54:39 +0200

    added control sequences file and entered raw mode

--- main.lua
+++ main.lua
@@ -5,3 +5,24 @@
 --   o size -> vector
 --   o buffer
 
+local paths = require "paths"
+local window = require(paths.window)
+require(paths.control_sequence)
+
+local w1 = window.new( {0, 0}, {0, 1} )
+
+-- set terminal in raw mode without echo
+os.execute("stty raw -echo")
+  
+-- main loop
+clear_screen()
+move_cursor_to( {1, 1} )
+local input = ""
+repeat
+  input = io.read(1)
+until input == "q"
+input = nil
+
+-- set terminal back in cooked mode with echo
+os.execute("stty cooked echo")
+
--- paths.lua
+++ paths.lua
@@ -5,6 +5,7 @@ local paths =  {}
 
 paths.vector = "src/vector"
 paths.window = "src/window"
+paths.control_sequence = "src/control_sequence"
 
 return paths
 
